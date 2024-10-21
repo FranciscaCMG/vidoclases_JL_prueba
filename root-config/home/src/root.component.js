b5c7daf9101eb08e41c0a1ba8c501aac386f63c3
FranciscaCMG <francisca.marquez@usach.cl>
2024-10-21 00:33:22 -0300

    se agrega el microservicio de navbar y el home, tambien se implementan las rutas

--- root-config/home/src/root.component.js
+++ root-config/home/src/root.component.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Singin from './components/Singin';
+import Home from './components/Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function Root(props) {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Singin" element={<Singin />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- root-config/login/src/components/Home.jsx
+++ root-config/login/src/components/Home.jsx
@@ -2,7 +2,6 @@ export function Home () {
     return (
         <div>
             <h1>Bienvenido/a</h1>
-            
         </div>
     )
 }
